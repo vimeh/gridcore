80db2e48c3fe74e4eb29365d143a0825e56c52fb
Vinay Mehta <hi@vinaym.com>
2025-08-16 12:31:28 -0700

    remove unused rendering code from ui

--- gridcore-rs/gridcore-ui/src/rendering/mod.rs
+++ gridcore-rs/gridcore-ui/src/rendering/mod.rs
@@ -1,7 +1,3 @@
-pub mod render_cache;
 pub mod theme;
-pub mod virtual_scroll;
 
-pub use render_cache::{CellKey, CellRenderData, RenderCache};
 pub use theme::{default_theme, GridTheme};
-pub use virtual_scroll::{CellRange, VirtualScrollManager};
